64d226ee496c903726577a6760787dc387f4156f
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-05-21 16:28:06 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.larrykin.jepschemistpos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.larrykin.jepschemistpos to javafx.fxml;
+    exports com.larrykin.jepschemistpos;
+}
