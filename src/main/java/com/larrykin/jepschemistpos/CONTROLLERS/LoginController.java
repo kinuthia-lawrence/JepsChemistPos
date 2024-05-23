02094ea4e0df370f8cf53e75b04033a3fe59166d
kinuthia lawrence <kinuthialawrence343@gmail.com>
2024-05-23 16:50:28 +0300

    modified the login  user interface

--- src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/LoginController.java
+++ src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/LoginController.java
@@ -1,4 +1,43 @@
 package com.larrykin.jepschemistpos.CONTROLLERS;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginController {
+
+    @FXML
+    private AnchorPane anchorPaneLeft;
+
+    @FXML
+    private AnchorPane anchorPaneRight;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Hyperlink forgotPasswordHyperLink;
+
+    @FXML
+    private AnchorPane imageAnchorPane;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private PasswordField passwordField;
+
 }
