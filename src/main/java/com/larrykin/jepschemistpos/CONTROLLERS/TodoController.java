3cb2f845c243e896b8f9db7b0f5f7fc10a8bb91b
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-12-24 17:53:55 +0300

    handled the todo ui

--- src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/TodoController.java
+++ src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/TodoController.java
@@ -1,4 +1,26 @@
 package com.larrykin.jepschemistpos.CONTROLLERS;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableView;
+
 public class TodoController {
+
+    @FXML
+    private Button todoCancelButton;
+
+    @FXML
+    private TextArea todoDescription;
+
+    @FXML
+    private Button todoSaveButton;
+
+    @FXML
+    private TextField todoTitle;
+
+    @FXML
+    private TreeTableView<?> todosTable;
+
 }
