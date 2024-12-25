1e695b05ece7d407fb728f4832089b833e735031
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-12-25 21:00:16 +0300

    modified help controller --to load the Help.md as a stream

--- src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/HelpController.java
+++ src/main/java/com/larrykin/jepschemistpos/CONTROLLERS/HelpController.java
@@ -1,6 +1,5 @@
 package com.larrykin.jepschemistpos.CONTROLLERS;
 
-
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.ast.Document;
@@ -9,11 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class HelpController implements Initializable {
 
@@ -22,8 +23,12 @@ public class HelpController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            String markdownContent = new String(Files.readAllBytes(Paths.get("HELP.md")));
+        try (InputStream inputStream = getClass().getResourceAsStream("/HELP.md")) {
+            if (inputStream == null) {
+                throw new IOException("HELP.md file not found");
+            }
+            String markdownContent = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines().collect(Collectors.joining("\n"));
             Parser parser = Parser.builder().build();
             Document document = parser.parse(markdownContent);
             HtmlRenderer renderer = HtmlRenderer.builder().build();
@@ -34,7 +39,7 @@ public class HelpController implements Initializable {
             webView.getEngine().setUserStyleSheetLocation(getClass().getResource("/STYLES/dark-mode.css").toString());
 
             helpAnchorPane.getChildren().add(webView);
-            //? Set the AnchorPane constraints to the WebView
+            // Set the AnchorPane constraints to the WebView
             AnchorPane.setTopAnchor(webView, 0.0);
             AnchorPane.setBottomAnchor(webView, 0.0);
             AnchorPane.setLeftAnchor(webView, 0.0);
